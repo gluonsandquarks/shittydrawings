3bfeaca2566049666c6ba6dd0b7be8a317f87e8b
VITRIOL <kevinlop34@hotmail.com>
2019-03-30 23:26:35 -0700

    Update _D1.js

--- _D1/_D1.js
+++ _D1/_D1.js
@@ -1,12 +1,9 @@
 function setup() {
-<<<<<<< HEAD
 	var cnv = createCanvas(100, 100);
 	var x = (windowWidth - width) / 2;
  	var y = (windowHeight - height) /2;
  	cnv.position(x, y);
-=======
   createCanvas(200, 200);
->>>>>>> a6fec74f53a3120399db5c7b6959a29e24476456
 }
 
 
