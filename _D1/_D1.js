da048a198e64d86758d4691ca71df7132abb32ac
VITRIOL <kevinlop34@hotmail.com>
2019-04-05 20:23:43 -0700

    Update _D1.js

--- _D1/_D1.js
+++ _D1/_D1.js
@@ -5,13 +5,13 @@ function setup() {
 	var x = (windowWidth - width) / 2;
  	var y = (windowHeight - height) /2;
  	cnv.position(x, y);
-  createCanvas(xlength, ylenght);
-
-  background(9, 29, 87);
+ 	createCanvas(xlength, ylenght);
+ 	background('#0f0036');
 }
 
 function draw() {
+	noStroke();
+	fill(255);
+	ellipse(width/2, height/4, 80, 80);
 
-  ellipse(width/2, height/2, 80, 80);
-  fill(255)
 }
