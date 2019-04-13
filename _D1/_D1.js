0de177695f6c2562b8fce367f3fb182f76625908
VITRIOL <kevinlop34@hotmail.com>
2019-04-12 23:39:31 -0700

    Update _D1.js

--- _D1/_D1.js
+++ _D1/_D1.js
@@ -1,24 +1,33 @@
 function setup() {
+	//canvas position and background color
 	var xlength = 600;
 	var ylenght = 600;
 	var cnv = createCanvas(xlength, ylenght);
  	cnv.position(0, 0);
  	createCanvas(xlength, ylenght);
  	background('#0f0036');
+
+	//stars
+ 	for (var i = 0; i<=300; i++) {
+
+		x = random(0, 600);
+    	y = random(0, 600);
+    	smooth(120);
+  		noStroke();
+		fill('#eeeeee');	
+		ellipse(x, y, 2, 2);
+		
+    }
 }
 
 function draw() {
+
+	//moon
 	var xmoon = 435;
 	var ymoon = 120;
 	smooth(1000);
 	noStroke();
 	fill('#ffff77');
     ellipse(xmoon, ymoon, 100, 100);
-    smooth(120);
-    noStroke();
-    fill('#eeeeee');
-    x = random(0, 600);
-    y = random(0, 600);
-    z = random(0, 600);
-   	ellipse(x, y, 2, 2);
+
 }
